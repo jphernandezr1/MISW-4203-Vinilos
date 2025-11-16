e4a5886858b779c042247a757237972bf9f6e01c
Nixon Ortiz <nf.ortiz@uniandes.edu.co>
2025-11-16 15:25:44 -0500

    add cache manager

---
+++ vinyls/app/src/main/java/com/example/vinyls/network/CacheManager.kt
@@ -0,0 +1,27 @@
+package com.example.vinyls.network
+
+import android.content.Context
+import com.example.vinyls.model.Collector
+
+class CacheManager(context: Context) {
+    companion object{
+        var instance: CacheManager? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: CacheManager(context).also {
+                    instance = it
+                }
+            }
+    }
+
+    private var collectors:  List<Collector> = mutableListOf()
+
+    fun addCollectors(newCollectors: List<Collector>){
+        if (collectors.isEmpty()){
+            collectors = newCollectors
+        }
+    }
+    fun getCollectors() : List<Collector>{
+        return collectors
+    }
+}
