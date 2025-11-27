c7afe5ef9204dcac033e7405da09619479fb6422
Nixon Ortiz <nf.ortiz@uniandes.edu.co>
2025-11-27 16:03:44 -0500

    added tests

--- vinyls/app/build.gradle.kts
+++ vinyls/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.navigation.testing)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
